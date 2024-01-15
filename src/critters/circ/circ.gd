c352c51ddfc7806adc3a605559dad2ff3ea96336
Bjørnar Prytz <bjornarprytz@gmail.com>
2024-01-15 20:15:58 +0100

    circ wip

--- src/critters/circ/circ.gd
+++ src/critters/circ/circ.gd
@@ -2,9 +2,7 @@ class_name Circ
 extends Critter
 
 
-func _on_dancing_state_physics_processing(delta: float) -> void:
-	pass # Replace with function body.
-
+var _target: Vector2
 
 func _on_singing_state_physics_processing(delta: float) -> void:
 	pass # Replace with function body.
@@ -20,3 +18,15 @@ func _on_approaching_state_physics_processing(delta: float) -> void:
 
 func _on_body_area_entered(area: Area2D) -> void:
 	pass # Replace with function body.
+
+
+func _on_planting_state_physics_processing(delta: float) -> void:
+	pass # Replace with function body.
+
+
+func _on_moving_state_physics_processing(delta: float) -> void:
+	pass # Replace with function body.
+
+
+func _set_random_target():
+	_target = global_position + (Global.random_vector2() * randf_range(180.0, 240.0))
